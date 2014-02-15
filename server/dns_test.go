2ab36326117131276b5efcdbe4f64e135e3493f3
Miek Gieben <miek@miek.nl>
2014-02-15 13:00:47 +0000

    Add the start of DNS DNSSEC compliance testing

---
+++ server/dns_test.go
@@ -0,0 +1,12 @@
+// Copyright (c) 2013 The SkyDNS Authors. All rights reserved.
+// Use of this source code is governed by The MIT License (MIT) that can be
+// found in the LICENSE file.
+
+package server
+
+// This test file tests DNS and DNSSEC compliance and is different from
+// the other tests in server_test.go which mostly check functionality.
+// I'm grateful to Peter van Dijk of PowerDNS fame for copying the testsuite
+// of PowerDNS to SkyDNS. See https://github.com/PowerDNS/pdns .
+
+
